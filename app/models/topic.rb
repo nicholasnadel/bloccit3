bd2e92d45531d905e771519e585d3e0c0922f66d
nicholasnadel <nicholasnadel@gmail.com>
2014-10-30 12:48:05 -0700

    Private Topics Checpoint. RSpec passed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
+  scope :public, -> { where(public: true) }
+  scope :private, -> { where(public: false) }
 end
